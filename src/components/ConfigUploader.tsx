21e390f9a670ee4adbef2be9c819c301725336ee
awhitmana0 <andrew.whitman@okta.com>
2025-09-22 20:46:28 -0700

    Update ConfigUploader.tsx

--- src/components/ConfigUploader.tsx
+++ src/components/ConfigUploader.tsx
@@ -248,6 +248,8 @@ export default function ConfigUploader({ onConfigLoad }: ConfigUploaderProps) {
             </div>
           )}
         </div>
+        </div>
+        </div>
 
         {/* Right Column - Callback URL */}
         <div className="space-y-4">
